2dc40bcbea37eb6177bc60b029176e51be0979e2
apple <apple@apples-MacBook-Pro-2.local>
2025-03-14 19:40:28 +0530

    dvhj

---
+++ next.config.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+
+
+module.exports = {
+  env: {
+    MONGO_URL: process.env.MONGO_URL,
+    JWT_SECRET: process.env.JWT_SECRET,
+    TWILIO_ACCOUNT_SID: process.env.TWILIO_ACCOUNT_SID,
+    TWILIO_AUTH_TOKEN: process.env.TWILIO_AUTH_TOKEN,
+    TWILIO_PHONE_NUMBER: process.env.TWILIO_PHONE_NUMBER,
+    AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+    AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+    AWS_REGION: process.env.AWS_REGION,
+    AWS_BUCKET_NAME: process.env.AWS_BUCKET_NAME,
+  },
+};
+
+
