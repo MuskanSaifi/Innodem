96132aae554045770204a64cd1979b0c73f685eb
muskansaifi <saifimuskan484@gmail.com>
2025-04-15 17:33:11 +0530

    wgrywe

--- src/app/userdashboard/Userprofile.js
+++ src/app/userdashboard/Userprofile.js
@@ -40,7 +40,7 @@ const Userprofile = () => {
     return (
       <div className="flex flex-col items-center justify-center min-h-screen">
         <div className="w-12 h-12 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
-        <p className="mt-4 text-gray-500 text-lg">Loading user details...</p>
+        <p className="mt-4 text-gray-500 text-lg">Loading user details....</p>
       </div>
     );
   }
